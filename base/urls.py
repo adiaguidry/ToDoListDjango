d489d803e1212438f9db167970000c19fcb27394
adiaguidry <adiaguidry@gmail.com>
2023-04-17 10:34:09 -0600

    update setup tools

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PendingList, TaskDetail, CreateTask, EditTask, DeleteTask, Login, RegisterPage
 from django.contrib.auth.views import LogoutView
 
-urlpatterns = [path('', PendingList.as_view(), name='tasks'),
+urlpatterns = [path('/', PendingList.as_view(), name='tasks'),
                path('login/', Login.as_view(), name='login'),
                path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
                path('register/', RegisterPage.as_view(), name='register'),
